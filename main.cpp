51da6b55e934272f46530ce0eec8559b59e55c51
winstonjose01 <winston.jose@gmail.com>
2024-10-25 15:03:14 -0700

    revision-2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <algorithm>
 using namespace std;
 using namespace std::chrono;
 
@@ -34,12 +35,31 @@ int main() {
     auto start3 = high_resolution_clock::now();
     set<string> randomIDs_set;
     while (getline(fin, line))
-        randomIDs_list.push_back(line);
+        randomIDs_set.emplace(line);
     auto end3 = high_resolution_clock::now();
     auto duration3 = duration_cast<milliseconds>(end3-start3);
     cout << "Reading codes into a list: " << duration3.count() << " milliseconds\n";
 
+    cout << "--------------------- SORTING-------------------------------\n";
+    auto start4 = high_resolution_clock::now();
+    sort(randomIDs_vector.begin(), randomIDs_vector.end());
+    auto end4 = high_resolution_clock::now();
+    auto duration4 = duration_cast<milliseconds>(end4-start4);
+    cout << "Sort codes in the vector: " << duration4.count() << " milliseconds\n";
 
+    auto start5 = high_resolution_clock::now();
+    randomIDs_list.sort();
+    auto end5 = high_resolution_clock::now();
+    auto duration5 = duration_cast<milliseconds>(end5-start5);
+    cout << "Sort codes in the list: " << duration5.count() << " milliseconds\n";
+
+    auto start6 = high_resolution_clock::now();
+    bool 
+    randomIDs_set
+    auto end6 = high_resolution_clock::now();
+    auto duration6 = duration_cast<milliseconds>(end6-start6);
+    cout << "Sort codes in the set: " << duration6.count() << " milliseconds\n";
+    
     return 0;
 }
 
